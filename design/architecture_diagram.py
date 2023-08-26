01dbc1966d2e42f29f469a2a879ae9a0b25f6566
Sebastian Czech <sebaczech@gmail.com>
2023-08-26 08:03:26 +0200

    Update diagram

--- design/architecture_diagram.py
+++ design/architecture_diagram.py
@@ -4,7 +4,7 @@ from diagrams.aws.compute import Lambda
 from diagrams.aws.management import Cloudwatch
 from diagrams.aws.security import IAMRole
 
-with Diagram("Architecture diagram", show=False, direction="LR"):
+with Diagram("Architecture diagram", direction="LR"):
     user = Tablet("end user's web browser")
 
     with Cluster("AWS"):
@@ -12,5 +12,5 @@ with Diagram("Architecture diagram", show=False, direction="LR"):
         lambda_python_app = Lambda("Lambda\n(Python web app)")
         cloud_watch_logs = Cloudwatch("CloudWatch logs\n(from web app)")
 
-    user >> lambda_python_app >> cloud_watch_logs
-    lambda_python_app - Edge(color="brown", style="dashed") - iam_role
+    user >> Edge(color="black", label="executes\n\n") >> lambda_python_app >> Edge(color="black", label="sends logs") >> cloud_watch_logs
+    lambda_python_app >> Edge(color="darkgrey", style="dotted", label="has permissions\n\n") >> iam_role
