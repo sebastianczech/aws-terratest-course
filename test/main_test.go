5eeb9689be8325130bd42c9dff91d3a4953f1ca3
Sebastian Czech <sebaczech@gmail.com>
2023-09-02 20:56:30 +0200

    Introduce failures in code

--- test/main_test.go
+++ test/main_test.go
@@ -14,21 +14,13 @@ func TestAwsInfra(t *testing.T) {
 		// Set the path to the Terraform code that will be tested.
 		TerraformDir: "../infra",
 		// Variables to pass to our Terraform code using -var options
-		Vars: map[string]interface{}{
-			"prefix": "terratest",
-		},
+		Vars: map[string]interface{}{},
 	})
 
-	// Clean up resources with "terraform destroy" at the end of the test.
-	defer terraform.Destroy(t, terraformOptions)
-
 	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
-	// terraform.InitAndApply(t, terraformOptions)
-	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
-	// After that it runs plan again and will fail the test if plan requires additional changes.
-	terraform.InitAndApplyAndIdempotent(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
 
 	// Run `terraform output` to get the values of output variables and check they have the expected values.
-	output := terraform.Output(t, terraformOptions, "lambda_url")
+	output := terraform.Output(t, terraformOptions, TODO)
 	assert.Contains(t, output, "lambda-url")
 }
